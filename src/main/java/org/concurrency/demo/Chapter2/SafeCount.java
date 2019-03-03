286a6dc976f7616f0c944c525261763e915e1677
cuixiuyin <1099442418@qq.com>
2019-03-03 18:30:07 +0800

    Chapter2

---
+++ src/main/java/org/concurrency/demo/Chapter2/SafeCount.java
@@ -0,0 +1,61 @@
+package org.concurrency.demo.Chapter2;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * @author xiuyin.cui
+ * @Description
+ * @date 2019/3/3 17:21
+ */
+public class SafeCount {
+
+    private AtomicInteger atomicI = new AtomicInteger(0);
+    private int i = 0;
+
+    public static void main(String[] args) {
+        final SafeCount cas = new SafeCount();
+        List<Thread> ts = new ArrayList<>(600);
+        long start = System.currentTimeMillis();
+        for (int j = 0; j < 100; j++) {
+            Thread t = new Thread(() -> {
+                for (int i = 0; i < 10000; i++) {
+                    cas.count();
+                    cas.safeCount();
+                }
+            });
+            ts.add(t);
+        }
+        for (Thread t : ts) {
+            t.start();
+        }
+        // 等待所有线程执行完成
+        for (Thread t : ts) {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        System.out.println(cas.i);
+        System.out.println(cas.atomicI.get());
+        System.out.println(System.currentTimeMillis() - start);
+    }
+
+    /**
+     * 使用CAS实现线程安全计数器
+     */
+
+    private void safeCount() {
+        atomicI.getAndIncrement();
+    }
+
+    /**
+     * 非线程安全计数器
+     */
+
+    private void count() {
+        i++;
+    }
+}
