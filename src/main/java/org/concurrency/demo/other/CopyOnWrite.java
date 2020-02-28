f08f834f5a518f357cee0202b36427c06c9b0a2d
cuixiuyin <1099442418@qq.com>
2020-02-28 14:43:27 +0800

    CopyOnWriteArrayList

---
+++ src/main/java/org/concurrency/demo/other/CopyOnWrite.java
@@ -0,0 +1,16 @@
+package org.concurrency.demo.other;
+
+import java.util.concurrent.CopyOnWriteArrayList;
+
+/**
+ * @author : cuixiuyin
+ * @date : 2020/2/27
+ */
+public class CopyOnWrite {
+
+    static CopyOnWriteArrayList copyOnWriteArrayList = new CopyOnWriteArrayList();
+
+    public static void main(String[] args) {
+
+    }
+}
