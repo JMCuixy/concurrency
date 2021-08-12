b23ddb219880730c36dd581f550feb2f4ed3eafc
xiuyin.cui <1099442418@qq.com>
2021-08-12 12:53:44 +0800

    guava 内存缓存
    Change-Id: Ifb0600f421e48a1bb039205c8e9d7d23bf311dfb

--- src/main/java/org/concurrency/demo/guava/CacheBuilderTest.java
+++ src/main/java/org/concurrency/demo/guava/CacheBuilderTest.java
@@ -1,11 +1,17 @@
 package org.concurrency.demo.guava;
 
-import com.google.common.cache.*;
+import com.google.common.cache.CacheBuilder;
+import com.google.common.cache.CacheLoader;
+import com.google.common.cache.CacheStats;
+import com.google.common.cache.LoadingCache;
+import com.google.common.cache.RemovalListener;
+import com.google.common.cache.RemovalNotification;
 import com.google.common.util.concurrent.ListenableFuture;
 import com.google.common.util.concurrent.ListenableFutureTask;
 
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * @Author xiuyin.cui@luckincoffee.com
@@ -15,18 +21,11 @@ import java.util.concurrent.TimeUnit;
  */
 public class CacheBuilderTest {
 
+    private static AtomicInteger atomicInteger = new AtomicInteger();
 
     public static void main(String[] args) throws InterruptedException {
-
         LoadingCache<String, String> loadingCache = CacheBuilder.newBuilder()
-                // expireAfterAccess: 当缓存项在指定的时间段内没有被读或写就会被回收。
-                // expireAfterWrite：当缓存项在指定的时间段内没有更新就会被回收。为了避免缓存雪崩，guava 会限制只有一个加载
-                // 操作时进行加锁，其他请求必须阻塞等待这个加载操作完成。而且，在加载完成之后，其他请求的线程会逐一获得锁，
-                // 去判断是否已被加载完成，每个线程必须轮流地走一个“获得锁，获得值，释放锁”的过程，这样性能会有一些损耗。
-                // refreshAfterWrite：当缓存项上一次更新操作之后的多久会被刷新。在 refresh 的过程中，guava 会限制只有一个加
-                // 载操作时进行加锁，而其他查询先返回旧值，这样能有效减少等待和锁争用，所以 refreshAfterWrite 会比 expireAfterWrite
-                // 性能好。但它也有一个缺点，在吞吐量很低的情况下，如很长一段时间内没有查询之后，发生的查询有可能会得到一个旧值
-                // （这个旧值可能来自于很长时间之前）
+                .expireAfterWrite(60, TimeUnit.SECONDS)
                 .refreshAfterWrite(30, TimeUnit.SECONDS)
                 .maximumSize(10)
                 .removalListener(new RemovalListener() {
@@ -37,17 +36,19 @@ public class CacheBuilderTest {
                 })
                 .recordStats()
                 .build(new CacheLoader<String, String>() {
+
                     @Override
                     public String load(String key) throws Exception {
+
                         System.out.println("key:" + key + ", load");
-                        return "Hello Guava Cache " + key;
+                        return "Hello Guava Cache " + key + "load";
                     }
 
                     @Override
                     public ListenableFuture<String> reload(String key, String oldValue) throws Exception {
                         ListenableFutureTask<String> listenableFutureTask = ListenableFutureTask.create(() -> {
                             System.out.println("key:" + key + ", reload");
-                            return "Hello Guava Cache " + key;
+                            return "Hello Guava Cache " + key + "reload" + atomicInteger.incrementAndGet();
                         });
                         CompletableFuture.runAsync(listenableFutureTask);
                         return listenableFutureTask;
@@ -61,9 +62,8 @@ public class CacheBuilderTest {
             System.out.println("缓存命中率:" + stats.hitCount());
             System.out.println("加载新值的平均时间，单位为毫秒:" + stats.averageLoadPenalty() / 10000);
             System.out.println("缓存项被回收的总数，不包括显式清除:" + stats.evictionCount());
-            Thread.sleep(60000);
+            Thread.sleep(65000);
             System.out.println("---------------------------------------------------------");
         }
-
     }
 }
