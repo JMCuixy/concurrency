9ecad7862f9a93a843c80489d2bed8891c10c1c1
xiuyin.cui <1099442418@qq.com>
2021-09-11 09:53:29 +0800

    递归思维

---
+++ src/main/java/org/algorithm/recursive/Test1.java
@@ -0,0 +1,32 @@
+package org.algorithm.recursive;
+
+/**
+ * @Author: xiuyin.cui@joymo.tech
+ * @Date: 2021/9/10 17:59
+ * @Description: >
+ * 汉诺塔问题是源于印度一个古老传说的益智玩具。大梵天创造世界的时候做了三根金刚石柱子，在一根柱子上从下往上按照大小顺
+ * 序摞着 64 片黄金圆盘。大梵天命令婆罗门把圆盘从下面开始按大小顺序重新摆放在另一根柱子上，并且规定，在小圆盘上不能放大圆盘，在三根
+ * 柱子之间一次只能移动一个圆盘。
+ */
+public class Test1 {
+
+    public static void main(String[] args) {
+        String x = "x";
+        String y = "y";
+        String z = "z";
+        hanio(3, x, y, z);
+    }
+
+    public static void hanio(int n, String x, String y, String z) {
+        if (n < 1) {
+            System.out.println("汉诺塔的层数不能小于1");
+        } else if (n == 1) {
+            System.out.println("移动: " + x + " -> " + z);
+            return;
+        } else {
+            hanio(n - 1, x, z, y);
+            System.out.println("移动: " + x + " -> " + z);
+            hanio(n - 1, y, x, z);
+        }
+    }
+}
